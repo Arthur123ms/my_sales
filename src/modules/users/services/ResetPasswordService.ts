04c176044a159f2d413239ded3c5b4bb6997e6e9
Arthur123ms <arthur123souza2004@outlook.com>
2025-12-03 20:23:29 -0300

    feat: forgotcontroler, resetcontroller

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,37 @@
+import { userTokenRepositories } from '../database/repositories/UserTokensRepositories';
+import { usersRespositories } from '../database/repositories/UserRepositories';
+import AppError from 'src/shared/errors/appError';
+import { isAfter, addHours } from 'date-fns';
+import { hash } from 'bcrypt';
+
+interface IResetPassword {
+  token: string;
+  password: string;
+}
+
+export default class ResetPasswordService {
+  async execute({ token, password }: IResetPassword): Promise<void> {
+    const userToken = await userTokenRepositories.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User not found', 404);
+    }
+
+    const user = await usersRespositories.findById(userToken.id);
+
+    if (!user) {
+      throw new AppError('User not found', 404);
+    }
+
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('User not found', 404);
+    }
+
+    user.password = await hash(password, 10);
+
+    await usersRespositories.save(user);
+  }
+}
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,9 @@
 import productRouter from '@moodules/products/routes/ProductRoutes';
 import userRouter from '@moodules/users/routes/UserRoutes';
 import sessionRouter from '@moodules/users/routes/SessionRoutes';
-import { Router } from 'express';
+import express, { Router } from 'express';
 import avatarRouter from '@moodules/users/routes/AvatarRoutes';
+import uploadConfig from '@config/upload'
 
 const routes = Router();
 
@@ -13,5 +14,6 @@ routes.use('/products', productRouter)
 routes.use('/users', userRouter)
 routes.use('/session', sessionRouter)
 routes.use('/avatar', avatarRouter)
+routes.use('/files', express.static(uploadConfig.directory))
 
 export default routes;

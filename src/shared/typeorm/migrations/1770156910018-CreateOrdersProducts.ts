c1c514d22d281dd50b5b33f6111f8e861560fa9b
Arthur123ms <arthur123souza2004@outlook.com>
2026-02-03 19:42:40 -0300

    feat: created migrations Orders and Products

---
+++ src/shared/typeorm/migrations/1770156910018-CreateOrdersProducts.ts
@@ -0,0 +1,49 @@
+import { networkInterfaces } from 'os';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateOrdersProducts1770156910018 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders_products',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+
+          {
+            name: 'quantity',
+            type: 'int',
+          },
+
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+
+          {
+            name: 'update_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders_products')
+  }
+}
